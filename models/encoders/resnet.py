[Fix] ResNet apply Layer Normalization

models/encoders/resnet.py
@@ -11,6 +11,7 @@ class ResNet(nn.Module):
                  norm_layer=None, input_channels=1):
         super(ResNet, self).__init__()
         if norm_layer is None:
+            # norm_layer = BatchNorm2d
             norm_layer = LayerNorm2d
         self._norm_layer = norm_layer
 
@@ -27,7 +28,8 @@ class ResNet(nn.Module):
         self.base_width = width_per_group
         self.conv1 = nn.Conv2d(input_channels, self.inplanes, kernel_size=7, stride=2, padding=3,
                                bias=False)
-        self.bn1 = norm_layer(self.inplanes)
+        # self.bn1 = norm_layer(self.inplanes)
+        self.bn1 = norm_layer()
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         self.layer1 = self._make_layer(block, 64, layers[0])
@@ -67,7 +69,8 @@ class ResNet(nn.Module):
         if stride != 1 or self.inplanes != planes * block.expansion:
             downsample = nn.Sequential(
                 conv1x1(self.inplanes, planes * block.expansion, stride),
-                norm_layer(planes * block.expansion),
+                # norm_layer(planes * block.expansion),
+                norm_layer()
             )
 
         layers = []