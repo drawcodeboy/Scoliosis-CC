b96a15d96ba6aad437732afeb88966efa216a799
drawcodeboy <20rnjsekdns@naver.com>
2025-03-26 12:16:36 +0900

    [Add] refine repo

--- utils/engine.py
+++ utils/engine.py
@@ -11,9 +11,11 @@ def train_one_epoch(model, dataloader, optimizer, loss_fns, scheduler, device):
     total_cluster_loss = []
     
     for batch_idx, ((x_i, x_j), _) in enumerate(dataloader, start=1):
+        optimizer.zero_grad()
+        
         x_i = x_i.to(device)
         x_j = x_j.to(device)
-        
+            
         z_i, z_j, c_i, c_j = model(x_i, x_j)
         
         loss_instance = loss_fns['instance'](z_i, z_j)
